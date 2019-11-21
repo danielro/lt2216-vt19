9a20f20f1e91a8738a40479b8d3daa67ce668991
metriska <daniel@metriska.se>
2019-11-21 21:22:39 +0100

    a

--- vxml-server/app.py
+++ vxml-server/app.py
@@ -3,16 +3,30 @@ app = Flask(__name__)
 
 temp = '13'
 
-@app.route('/weather')
-def hello():
-    vxml = render_template('weather.xml', temp=temp)
+@app.route('/lab1')
+def lab1():
+    vxml = render_template('lab1/lab1.xml')
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
 
-@app.route('/lab1')
-def lab1():
-    vxml = render_template('lab1.xml')
+@app.route('/lab1_book')
+def lab1_book():
+    vxml = render_template('lab1/lab1_book.xml')
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
+@app.route('/lab1_delays')
+def lab1_delays():
+    vxml = render_template('lab1/lab1_delays.xml')
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
+@app.route('/lab1_menu')
+def lab1_menu():
+    vxml = render_template('lab1/lab1_menu.xml')
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
